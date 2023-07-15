1e6b89f66085248036a0033ab5825e8e7da3f2db
Zack <69579338+king101-bit@users.noreply.github.com>
2023-07-15 18:30:20 +0100

    Hotfix main.py (#5)
    - Added error handling for wrong format and other functions

--- main.py
+++ main.py
@@ -19,102 +19,81 @@ def track_phone_number(phone_number):
     # Print DAVID in ASCII art
     print(pyfiglet.figlet_format('DAVID'))
 
-    # Validate phone number
     try:
+        # Validate phone number
         parsed_number = phonenumbers.parse(phone_number, None)
     except phonenumbers.NumberParseException:
         print('Invalid phone number')
         return
 
     # Get geo location
-    geo_location = geocoder.description_for_number(parsed_number, 'en')
-    print('Geo Location: {}'.format(geo_location))
+    try:
+        geo_location = geocoder.description_for_number(parsed_number, 'en')
+        print('Geo Location: {}'.format(geo_location))
+    except Exception as e:
+        print('Error fetching geo location:', e)
 
     # Get country code
-    country_code = phonenumbers.region_code_for_number(parsed_number)
-    print('Country Code: {}'.format(country_code))
+    try:
+        country_code = phonenumbers.region_code_for_number(parsed_number)
+        print('Country Code: {}'.format(country_code))
+    except Exception as e:
+        print('Error fetching country code:', e)
 
     # Get area code
-    if parsed_number.country_code != 0:
-        area_code = phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.NATIONAL)[1:]
-        print('Area Code: {}'.format(area_code))
-    else:
-        print('Area Code: Not available for this number!')
+    try:
+        if parsed_number.country_code != 0:
+            area_code = phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.NATIONAL)[1:]
+            print('Area Code: {}'.format(area_code))
+        else:
+            print('Area Code: Not available for this number!')
+    except Exception as e:
+        print('Error fetching area code:', e)
 
     # Get line type
-    number_type = phonenumbers.number_type(parsed_number)
-    print('Line Type: {}'.format(number_type))
+    try:
+        number_type = phonenumbers.number_type(parsed_number)
+        print('Line Type: {}'.format(number_type))
+    except Exception as e:
+        print('Error fetching line type:', e)
 
     # Get carrier information
-    carrier_info = carrier.name_for_number(parsed_number, 'en')
-    print('Carrier: {}'.format(carrier_info))
+    try:
+        carrier_info = carrier.name_for_number(parsed_number, 'en')
+        print('Carrier: {}'.format(carrier_info))
+    except Exception as e:
+        print('Error fetching carrier information:', e)
 
     # Get timezone
-    timezone_name = timezone.time_zones_for_number(parsed_number)[0]
-    timezone_object = pytz.timezone(timezone_name)
+    try:
+        timezone_name = timezone.time_zones_for_number(parsed_number)[0]
+        timezone_object = pytz.timezone(timezone_name)
 
-    # Get current time and date of the location
-    timezone_datetime = datetime.now(timezone_object)
-    print('Current time and date: {}'.format(timezone_datetime))
+        # Get current time and date of the location
+        timezone_datetime = datetime.now(timezone_object)
+        print('Current time and date: {}'.format(timezone_datetime))
+    except Exception as e:
+        print('Error fetching timezone:', e)
 
     # Perform reverse phone number lookup
-    reverse_lookup(parsed_number)
+    try:
+        reverse_lookup(parsed_number)
+    except Exception as e:
+        print('Error performing reverse lookup:', e)
 
     # Check if phone number is pwned using HIBP API
-    check_pwned(phone_number)
+    try:
+        check_pwned(phone_number)
+    except Exception as e:
+        print('Error checking if phone number is pwned:', e)
 
 def reverse_lookup(parsed_number):
-    """
-    This function performs a reverse phone number lookup and provides information associated with the phone number.
-
-    Args:
-        parsed_number (phonenumbers.PhoneNumber): Parsed phone number object
-
-    Returns:
-        None
-    """
-    if parsed_number.country_code != 0:
-        phone_number = phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.E164)
-        url = f'https://api.telnyx.com/v1/phone_number/{phone_number}'
-        headers = {'User-Agent': 'OSINT Tool'}
-        response = requests.get(url, headers=headers)
-
-        if response.status_code == 200:
-            data = response.json()
-            if 'national_destination_code' in data:
-                destination_code = data['national_destination_code']
-                print('Destination Code: {}'.format(destination_code))
-            if 'carrier' in data:
-                carrier = data['carrier']
-                print('Carrier: {}'.format(carrier))
-        else:
-            print('Failed to perform reverse phone number lookup. Please try again later.')
-    else:
-        print('Reverse phone number lookup is not available for this number.')
+    # The reverse_lookup function remains the same as in your original code
+    # ...
 
 def check_pwned(phone_number):
-    """
-    This function checks if a phone number has been compromised in known data breaches using the HIBP API.
-
-    Args:
-        phone_number (str): Phone number in the format +1-XXX-XXX-XXXX
-
-    Returns:
-        None
-    """
-    url = f'https://haveibeenpwned.com/api/v3/breachedaccount/{phone_number}'
-    headers = {'User-Agent': 'OSINT Tool'}
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        print('This phone number has been pwned. It has appeared in the following data breaches:')
-        data_breaches = response.json()
-        for breach in data_breaches:
-            print(' - {}'.format(breach['Title']))
-    elif response.status_code == 404:
-        print('This phone number has not been pwned.')
-    else:
-        print('Failed to check if the phone number is pwned. Please try again later.')
+    # The check_pwned function remains the same as in your original code
+    # ...
 
 if __name__ == '__main__':
     phone_number = input('Enter phone number in the format +1-XXX-XXX-XXXX: ')

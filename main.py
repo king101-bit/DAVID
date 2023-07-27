27528d5c45f8056365b473bc04cf8f1c9718649d
Zack <69579338+king101-bit@users.noreply.github.com>
2023-07-27 15:14:30 +0100

    Revert main.py
    Reverted code back to non truecaller api

--- main.py
+++ main.py
@@ -3,28 +3,89 @@ import phonenumbers
 from phonenumbers import geocoder, timezone, carrier
 from datetime import datetime
 import pytz
-import requests
 
-# Define the Truecaller API endpoint
-TRUECALLER_API_URL = "https://api4.truecaller.com/v1/lookup"
-
-def get_truecaller_api_key():
+def track_phone_number(phone_number):
     """
-    Function to prompt the user for their Truecaller API key.
+    This function takes a phone number in the format +1-XXX-XXX-XXXX and performs various OSINT operations.
+    It prints DAVID in ASCII art, tracks the phone number, and provides detailed information about it.
+
+    Args:
+        phone_number (str): Phone number in the format +1-XXX-XXX-XXXX
 
     Returns:
-        str: The user's Truecaller API key.
+        None
     """
-    api_key = input("Enter your Truecaller API key: ")
-    return api_key
+    # Print DAVID in ASCII art
+    print(pyfiglet.figlet_format('DAVID'))
 
-def reverse_lookup(parsed_number, api_key):
+    try:
+        # Validate phone number
+        parsed_number = phonenumbers.parse(phone_number, None)
+    except phonenumbers.NumberParseException:
+        print('Invalid phone number')
+        return
+
+    # Get geo location
+    try:
+        geo_location = geocoder.description_for_number(parsed_number, 'en')
+        print('Geo Location: {}'.format(geo_location))
+    except Exception as e:
+        print('Error fetching geo location:', e)
+
+    # Get country code
+    try:
+        country_code = phonenumbers.region_code_for_number(parsed_number)
+        print('Country Code: {}'.format(country_code))
+    except Exception as e:
+        print('Error fetching country code:', e)
+
+    # Get area code
+    try:
+        if parsed_number.country_code != 0:
+            area_code = phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.NATIONAL)[1:]
+            print('Area Code: {}'.format(area_code))
+        else:
+            print('Area Code: Not available for this number!')
+    except Exception as e:
+        print('Error fetching area code:', e)
+
+    # Get line type
+    try:
+        number_type = phonenumbers.number_type(parsed_number)
+        print('Line Type: {}'.format(number_type))
+    except Exception as e:
+        print('Error fetching line type:', e)
+
+    # Get carrier information
+    try:
+        carrier_info = carrier.name_for_number(parsed_number, 'en')
+        print('Carrier: {}'.format(carrier_info))
+    except Exception as e:
+        print('Error fetching carrier information:', e)
+
+    # Get timezone
+    try:
+        timezone_name = timezone.time_zones_for_number(parsed_number)[0]
+        timezone_object = pytz.timezone(timezone_name)
+
+        # Get current time and date of the location
+        timezone_datetime = datetime.now(timezone_object)
+        print('Current time and date: {}'.format(timezone_datetime))
+    except Exception as e:
+        print('Error fetching timezone:', e)
+
+    # Perform reverse phone number lookup using phonenumbers package
+    try:
+        reverse_lookup(parsed_number)
+    except Exception as e:
+        print('Error performing reverse lookup:', e)
+
+def reverse_lookup(parsed_number):
     """
-    Performs a reverse phone number lookup using the Truecaller API.
+    Performs a reverse phone number lookup using the phonenumbers package.
 
     Args:
         parsed_number (phonenumbers.PhoneNumber): Parsed phone number object.
-        api_key (str): User's Truecaller API key.
 
     Returns:
         None
@@ -32,37 +93,13 @@ def reverse_lookup(parsed_number, api_key):
     try:
         # Get the phone number in international format (e.g., +1XXXXXXXXXX)
         phone_number = phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.E164)
-
-        # Make the API request
-        params = {
-            "phoneNumber": phone_number,
-            "countryCode": str(parsed_number.country_code),
-            "apikey": api_key,
-        }
-
-        response = requests.get(TRUECALLER_API_URL, params=params)
-        data = response.json()
-
-        # Extract and print information from the API response
-        if data["spam"] is False:
-            print("Number not marked as spam on Truecaller.")
-            if data["name"]:
-                print("Name: {}".format(data["name"]))
-            if data["city"]:
-                print("City: {}".format(data["city"]["name"]))
-            if data["carrier"]:
-                print("Carrier: {}".format(data["carrier"]["name"]))
-        else:
-            print("Number marked as spam on Truecaller.")
+        
+        # Replace this section with your preferred reverse lookup implementation
+        # For this example, we'll print the international phone number.
+        print("Reverse Lookup: {}".format(phone_number))
     except Exception as e:
         print("Error performing reverse lookup:", e)
 
-# Rest of the code remains the same...
-
 if __name__ == '__main__':
-    # Get Truecaller API key from the user during installation
-    truecaller_api_key = get_truecaller_api_key()
-
-    # Now you can use the obtained API key for reverse lookup
     phone_number = input('Enter phone number in the format +1-XXX-XXX-XXXX: ')
-    track_phone_number(phone_number, truecaller_api_key)
+    track_phone_number(phone_number)
